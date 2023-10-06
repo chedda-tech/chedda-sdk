868b73cdf086c5186f9820a5681e8e911365ff4b
sadeeqdev <boskitechh@gmail.com>
2023-10-06 16:49:16 +0100

    feat: add more vault test

---
+++ src/utils/mocks.ts
@@ -0,0 +1,28 @@
+import { CheddaVault } from '../vault'
+
+export const mockCheddaVault: Partial<CheddaVault> = {
+  depositAsset: jest.fn(),
+  redeem: jest.fn(),
+  addCollateral: jest.fn(),
+  removeCollateral: jest.fn(),
+  accountCollateralTokenIds: jest.fn(),
+  borrow: jest.fn(),
+  repay: jest.fn(),
+  collateral: jest.fn(),
+  totalAssets: jest.fn(),
+  collateralAmounts: jest.fn(),
+  utilization: jest.fn(),
+  totalAccountCollateralValue: jest.fn(),
+  depositApr: jest.fn(),
+  borrowApr: jest.fn(),
+  rewardsApr: jest.fn(),
+  getVaultStats: jest.fn(),
+  approve: jest.fn(),
+  allowance: jest.fn(),
+  balanceOf: jest.fn(),
+  transfer: jest.fn(),
+  totalSupply: jest.fn(),
+  gauge: jest.fn(),
+  accountPendingAmount: jest.fn(),
+  initiateContract: jest.fn(),
+}
